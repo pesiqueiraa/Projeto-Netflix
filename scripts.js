c7ce81a307b3395e4c8574c72249ff84598d034c
pesiqueiraa <pedrosiqueirasouza6@gmail.com>
2023-08-22 20:36:50 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,17 @@
+let botao = document.getElementById("botao-pergunta");
+let menu = document.getElementById("pergunta");
+let imagem = document.getElementById("imagem-pergunta")
+
+function ativarBotao() {
+
+    if (menu.style.display == "none") {
+        menu.style.display = "block";
+        imagem.id = "imagem-pergunta-girar"
+    } else {
+        menu.style.display = "none"
+        imagem.id = "imagem-pergunta"
+    }
+
+}
+
+botao.addEventListener("click", ativarBotao);
